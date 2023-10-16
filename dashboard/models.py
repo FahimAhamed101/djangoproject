a53328496486f9ae89964ba7a9ac92126d0504a9
fahim <fahimahamedweb@gmail.com>
2023-10-16 22:07:17 +0600

    done backend

--- dashboard/models.py
+++ dashboard/models.py
@@ -22,4 +22,42 @@ class User(AbstractUser):
     )
     role = models.PositiveSmallIntegerField(choices=ROLES, blank=True, null=True)
 
+
+class Userinfo:
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    first_name    = models.CharField(max_length=200, unique=True)
+    last_name    = models.CharField(max_length=200, unique=True)
+    username    = models.CharField(max_length=200, unique=True)
+    email           = models.EmailField(max_length=100, unique=True)
+    description     = models.TextField(max_length=500, blank=True)
+    phonenumber           = models.IntegerField()
+    totaltransaction           = models.IntegerField()
+    phonenumber           = models.IntegerField()
+    totalammount  = models.IntegerField()
+    disputes  = models.IntegerField()
+    totaldisputesammount  = models.IntegerField()
+    accceptedtrans  = models.IntegerField()
+    declinetrans  = models.IntegerField()
+    charbackcountrate          = models.IntegerField()
+    volume  = models.IntegerField()
+    limit  = models.IntegerField()
+    declinetrans  = models.IntegerField()
+    created_date    = models.DateTimeField(auto_now_add=True)
+    modified_date   = models.DateTimeField(auto_now=True)
+    
+class Transaction:
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    Bin  = models.CharField(max_length=100)
+    Lastfour  = models.CharField(max_length=100)
+    ammount  = models.IntegerField()
+    payout  = models.CharField(max_length=100)
+    code  = models.IntegerField()
+    accepted  = models.IntegerField()
+    InternalTransactionid  = models.CharField(max_length=100)
+    created_at    = models.DateTimeField(auto_now_add=True)
+    
+    
+
+    
+    
     
--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.dashboard, name= 'dashboard'),
     path('login/', views.login_view, name='login_view'),
-    
+     path('data/', views.data_view, name='data_view'),
 ]
