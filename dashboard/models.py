71304b9e6193f6679039a7f300f4c29776eb8d7a
fahim <fahimahamedweb@gmail.com>
2023-10-19 02:04:15 +0600

    account added

--- dashboard/models.py
+++ dashboard/models.py
@@ -57,8 +57,21 @@ class Transaction(models.Model):
     InternalTransactionid  = models.CharField(max_length=100)
     created_at    = models.DateTimeField(auto_now_add=True)
     
-    
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(Account, on_delete=models.CASCADE)
+    address_line_1 = models.CharField(blank=True, max_length=100)
+    address_line_2 = models.CharField(blank=True, max_length=100)
+    profile_picture = models.ImageField(blank=True, upload_to='userprofile')
+    city = models.CharField(blank=True, max_length=20)
+    state = models.CharField(blank=True, max_length=20)
+    country = models.CharField(blank=True, max_length=20)
+
+    def __str__(self):
+        return self.user.first_name
+
+    def full_address(self):
+        return f'{self.address_line_1} {self.address_line_2}'
     
     
     
